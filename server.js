8358c2b327b68015d6281dee1273435959dd5c7b
proraj9088 <rp8735685@gmail.com>
2021-06-04 00:57:33 +0530

    file download done yes flag

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
 
 const connectDB = require("./config/db");
 connectDB();
@@ -13,6 +14,7 @@ app.set('view engine', 'ejs');
 
 app.use('/api/files',require('./routes/files'));
 app.use('/files',require('./routes/show'));
+app.use('/files/download',require('./routes/download'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
