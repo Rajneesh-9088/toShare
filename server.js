75f5a2a1a936d049205323756c949d90c0139e05
proraj9088 <rp8735685@gmail.com>
2021-06-03 22:54:37 +0530

    down routes created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 
@@ -7,7 +8,11 @@ const PORT = process.env.PORT || 3000;
 const connectDB = require("./config/db");
 connectDB();
 
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
 app.use('/api/files',require('./routes/files'));
+app.use('/files',require('./routes/show'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
